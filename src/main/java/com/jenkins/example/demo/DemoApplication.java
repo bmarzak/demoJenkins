8ec2130b823d01aecd7d0c7160d66dfab5616347
unknown <bmarzak@simplon.co>
2021-09-07 20:47:49 +0100

    first commit !

---
+++ src/main/java/com/jenkins/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.jenkins.example.demo;
+
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+	
+	public static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("Application Started ...");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application executed ...");
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
